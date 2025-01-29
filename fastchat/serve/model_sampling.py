5a8f95cc97f37faee5c988a52aa095788cd4fb1e
digitsisyph <ksqod_code@pm.me>
2025-01-29 22:56:31 +0800

    Fix model api base and vision

--- fastchat/serve/model_sampling.py
+++ fastchat/serve/model_sampling.py
@@ -44,7 +44,6 @@ VISION_SAMPLING_WEIGHTS = {
     "gpt-4o-mini-2024-07-18": 1,
     "gpt-4o-2024-08-06": 1,
     'qwen2.5-vl-72b-instruct': 1,
-    'claude-3-5-sonnet-20241022': 1,
     'claude-3-haiku-20240307': 1,
     # "gpt-4-0125-preview": 1,
     # "gemini-1.5-pro": 0.5,
